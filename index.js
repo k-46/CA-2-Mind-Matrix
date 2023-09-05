30cc598170711c26e1ae178d93533bb1ae9258ca
k-46 <kamalesh.a@kalvium.community>
2023-09-05 18:10:20 +0530

    started

---
+++ index.js
@@ -0,0 +1,10 @@
+const sub = document.querySelector("#su");
+const op = document.querySelector("#op").value;
+const nc = document.querySelector("#userName").value;
+
+sub.addEventListener("click",()=>{
+    console.log(userName.value,)
+    console.log(document.querySelector("form").difficulty.value)
+})
+
+console.log(document.querySelector("form").difficulty.value)
